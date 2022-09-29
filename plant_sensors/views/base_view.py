7e0d582e7431d3898c248d88d0fa4d1a54a20249
Taylor Jacovich <tejacovich@gmail.com>
2022-09-28 22:01:08 -0400

    Converted current_list.conf to json

--- plant_sensors/views/base_view.py
+++ plant_sensors/views/base_view.py
@@ -4,8 +4,11 @@ from flask_restful import Resource
 
 class BaseView(Resource):
     @classmethod
-    def get_sensor_list(self):
+    def get_sensor_list(self, type):
         sensor_list_file = current_app.config.get("SENSOR_LIST_FILE", current_app.root_path+"/sensor_files/current_list.conf")
+        sensor_list = {}
         with open(sensor_list_file, "r") as f:
-            sensor_list = {lst[0]: lst[1] for lst in f.readline().split('\t')}
+        for line in f:
+            sensor_description = json.loads(line)
+            sensor_list[sensor_description["name"]] = sensor_description["type"] 
         return sensor_list
