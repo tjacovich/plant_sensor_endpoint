f0fba6a356d93519c3450c5c92e85fbdcdbf710a
Taylor Jacovich <tejacovich@gmail.com>
2022-09-28 22:02:32 -0400

    Fixed indentation issue.

--- plant_sensors/views/base_view.py
+++ plant_sensors/views/base_view.py
@@ -8,7 +8,7 @@ class BaseView(Resource):
         sensor_list_file = current_app.config.get("SENSOR_LIST_FILE", current_app.root_path+"/sensor_files/current_list.conf")
         sensor_list = {}
         with open(sensor_list_file, "r") as f:
-        for line in f:
-            sensor_description = json.loads(line)
-            sensor_list[sensor_description["name"]] = sensor_description["type"] 
-        return sensor_list
+            for line in f:
+                sensor_description = json.loads(line)
+                sensor_list[sensor_description["name"]] = sensor_description["type"] 
+            return sensor_list
